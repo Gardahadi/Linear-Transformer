b5c02083bce5ff1c0e68e05dce5aa13b09ab42d4
wirasuta <wirasutat@gmail.com>
2018-11-09 01:12:59 +0700

    Added idle callback function

--- OpenGLUTTemplate.py
+++ OpenGLUTTemplate.py
@@ -5,82 +5,42 @@ from OpenGL.GLU import *
 
 window = 0                                             # glut window number
 width, height = 500, 400                               # window size
-anglePyramid = 0
+points = []
+rgb = [0.3,0.5,0.7]
 
-def draw_axis():
-    glBegin(GL_LINES)
+def draw_poly():
+    glBegin(GL_POLYGON)
 
-    glColor3f(10.0, 10.0, 10.0)    # White
-    glVertex3f(-10.0, 0.0, 0.0)    # Sb X
-    glVertex3f(10.0, 0.0, 0.0)    # Sb X
-
-    glColor3f(10.0, 10.0, 10.0)    # White
-    glVertex3f(0.0, -10.0, 0.0)    # Sb Y
-    glVertex3f(0.0, 10.0, 0.0)    # Sb Y
-
-    glColor3f(10.0, 10.0, 10.0)    # White
-    glVertex3f(0.0, 0.0, -10.0)    # Sb Z
-    glVertex3f(0.0, 0.0, 10.0)    # Sb Z
+    for point in points:
+        glVertex2f(point[0],point[1])
 
     glEnd()
 
-def draw_cube():
-    glBegin(GL_QUADS)                                  # start drawing a rectangle
-    # Top face (y = 0.5)
-    # Define vertices in counter-clockwise (CCW) order with normal pointing out
-    glColor3f(0.0, 1.0, 0.0)     # Green
-    glVertex3f( 0.5, 0.5, -0.5)
-    glVertex3f(-0.5, 0.5, -0.5)
-    glVertex3f(-0.5, 0.5,  0.5)
-    glVertex3f( 0.5, 0.5,  0.5)
-
-    # Bottom face (y = -0.5)
-    glColor3f(1.0, 0.5, 0.0)     # Orange
-    glVertex3f( 0.5, -0.5,  0.5)
-    glVertex3f(-0.5, -0.5,  0.5)
-    glVertex3f(-0.5, -0.5, -0.5)
-    glVertex3f( 0.5, -0.5, -0.5)
-
-    # Front face  (z = 0.5)
-    glColor3f(1.0, 0.0, 0.0)     # Red
-    glVertex3f( 0.5,  0.5, 0.5)
-    glVertex3f(-0.5,  0.5, 0.5)
-    glVertex3f(-0.5, -0.5, 0.5)
-    glVertex3f( 0.5, -0.5, 0.5)
-
-    # Back face (z = -0.5)
-    glColor3f(1.0, 1.0, 0.0)     # Yellow
-    glVertex3f( 0.5, -0.5, -0.5)
-    glVertex3f(-0.5, -0.5, -0.5)
-    glVertex3f(-0.5,  0.5, -0.5)
-    glVertex3f( 0.5,  0.5, -0.5)
-
-    # Left face (x = -0.5)
-    glColor3f(0.0, 0.0, 1.0)     # Blue
-    glVertex3f(-0.5,  0.5,  0.5)
-    glVertex3f(-0.5,  0.5, -0.5)
-    glVertex3f(-0.5, -0.5, -0.5)
-    glVertex3f(-0.5, -0.5,  0.5)
-
-    # Right face (x = 0.5)
-    glColor3f(1.0, 0.0, 1.0)     # Magenta
-    glVertex3f(0.5,  0.5, -0.5)
-    glVertex3f(0.5,  0.5,  0.5)
-    glVertex3f(0.5, -0.5,  0.5)
-    glVertex3f(0.5, -0.5, -0.5)
-    glEnd()
+def idle():
+    global points
+    cmd = input("$")
+    if cmd[0]=="e":
+        print(cmd)
+    elif cmd[0]=="a":
+        x,y = cmd.split(" ")[1].split(",")
+        x = float(x)
+        y = float(y)
+        points.append([x,y])
+    elif cmd[0]=="c":
+        r,g,b = cmd.split(" ")[1].split(",")
+        rgb[0] = float(r)
+        rgb[1] = float(g)
+        rgb[2] = float(b)
+    elif cmd[0]=="x":
+        exit()
+    glutPostRedisplay()
 
 def draw():                                            # ondraw is called all the time
     global anglePyramid
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen
     glLoadIdentity()                                   # reset position
-    gluLookAt(0, 0, 0, -100.0, -100.0, -100.0, 0.0,-1.0,0.0)
-    draw_cube()
-    glRotatef(anglePyramid, 0.0, 0.0, 1.0)
-    draw_rect()
-
-    anglePyramid += 0.01
-
+    glColor3f(rgb[0],rgb[1],rgb[2])
+    draw_poly()
     glutSwapBuffers()                                  # important for double buffering
 
 # initialization
@@ -88,8 +48,7 @@ glutInit()                                             # initialize glut
 glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
 glutInitWindowSize(width, height)                      # set window size
 glutInitWindowPosition(0, 0)                           # set window position
-window = glutCreateWindow("Boilerplate OpenGL")                  # create window with title
-glEnable(GL_DEPTH_TEST)                                # remove unseen faces
+window = glutCreateWindow("Boilerplate OpenGL")        # create window with title
 glutDisplayFunc(draw)                                  # set draw function callback
-glutIdleFunc(draw)                                     # draw all the time
+glutIdleFunc(idle)                                     # draw all the time
 glutMainLoop()
