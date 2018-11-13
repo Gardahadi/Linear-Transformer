6a53d3d0cc860fe2ced018e0e642d4dbb05daee1
wirasuta <wirasutat@gmail.com>
2018-11-13 15:22:11 +0700

    Added animation

--- OpenGLUTTemplate.py
+++ OpenGLUTTemplate.py
@@ -2,11 +2,22 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import time
 
 window = 0                                             # glut window number
 width, height = 500, 400                               # window size
-points = []
+points = [[0,0.5],[0.5,0],[0,-0.5],[-0.5,0]]
+pointsInit = [[0,0.5],[0.5,0],[0,-0.5],[-0.5,0]]
 rgb = [0.3,0.5,0.7]
+
+def draw_axis():
+    glBegin(GL_LINES)
+    glColor3f(1,1,1)
+    glVertex2f(-10,0)
+    glVertex2f(10,0)
+    glVertex2f(0,10)
+    glVertex2f(0,-10)
+    glEnd()
 
 def draw_poly():
     glBegin(GL_POLYGON)
@@ -17,7 +28,7 @@ def draw_poly():
     glEnd()
 
 def idle():
-    global points
+    global points, rgb
     cmd = input("$")
     if cmd[0]=="e":
         print(cmd)
@@ -31,9 +42,22 @@ def idle():
         rgb[0] = float(r)
         rgb[1] = float(g)
         rgb[2] = float(b)
+    elif cmd[0]=="t":
+        dx,dy = cmd.split(" ")[1].split(",")
+        dx = float(dx)
+        dy = float(dy)
+        frames = 60
+        for i in range(frames):
+            for p in points:
+                p[0] += dx/frames
+                p[1] += dy/frames
+            time.sleep(0.01)
+            draw()
+    elif cmd[0]=="r":
+        points = pointsInit
     elif cmd[0]=="x":
         exit()
-    glutPostRedisplay()
+    draw()
 
 def draw():                                            # ondraw is called all the time
     global anglePyramid
@@ -41,6 +65,7 @@ def draw():                                            # ondraw is called all th
     glLoadIdentity()                                   # reset position
     glColor3f(rgb[0],rgb[1],rgb[2])
     draw_poly()
+    draw_axis()
     glutSwapBuffers()                                  # important for double buffering
 
 # initialization
