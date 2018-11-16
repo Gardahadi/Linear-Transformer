37cb96fe63fe21d8c01dc6016efcbc3ad9ee756d
gardahadi <garda4199@gmail.com>
2018-11-16 21:31:03 +0700

    fixed quaternion.py

--- quaternion.py
+++ quaternion.py
@@ -1,19 +1,24 @@
 #Konvensi : A Quaternion shall be represented as a list with the following format
 # Q = S + Xi + Yj + Zk --> Q = [X,Y,Z,S]
+import math
+import copy
 
+p = [0,1,1,0]
+q=[0,math.sin(math.radians(45)),0,math.cos(math.radians(45))]
+q1=[0,-math.sin(math.radians(45)),0,math.cos(math.radians(45))]
 
 def productQ(Q1,Q2) :
   #Returns the product of two quaternions
 
-  TempQ1 = Q1
-  TempQ2 = Q2
+  TempQ1 = Q1.copy()
+  TempQ2 = Q2.copy()
   ReturnQ = []
   CrossProduct = []
   # Product 1 : S1 * S2
   P1 = Q1[3] * Q2[3]
 
   # Product 2 : V1 . V2
-  P2 = (Q1[0]*Q2[0])+(Q1[1]*Q2[1])+(Q[2]*Q[2])
+  P2 = (Q1[0]*Q2[0])+(Q1[1]*Q2[1])+(Q1[2]*Q2[2])
 
   # Product 3 : S1 * V2
   TempQ2[0] *= Q1[3]
@@ -29,3 +34,21 @@ def productQ(Q1,Q2) :
   CrossProduct.append(Q1[1]*Q2[2]-Q1[2]*Q2[1])
   CrossProduct.append(Q1[0]*Q2[2]-Q1[2]*Q2[0])
   CrossProduct.append(Q1[0]*Q2[1]-Q1[1]*Q1[0])
+
+  ReturnQ.append(TempQ1[0]+TempQ2[0]+CrossProduct[0])
+  ReturnQ.append(TempQ1[1]+TempQ2[1]+CrossProduct[1])
+  ReturnQ.append(TempQ1[2]+TempQ2[2]+CrossProduct[2])
+  ReturnQ.append(P1 - P2)
+
+  return ReturnQ
+
+def addQ (Q1,Q2) :
+
+    AddedQ = []
+
+    AddedQ.append(Q1[0]+Q2[0])
+    AddedQ.append(Q1[1]+Q2[1])
+    AddedQ.append(Q1[2]+Q2[2])
+    AddedQ.append(Q1[3]+Q2[3])
+
+    return AddedQ
